86cb32149e71943b821fe0932685b795ca131619
Sanjay Kumar <sanjay.kumar@tenpearls.com>
2020-11-02 18:06:27 +0500

    web pack add

---
+++ index.js
@@ -0,0 +1,10 @@
+
+export default function test(){
+    console.log('');
+    console.log('testing');
+console.log('testing new branch');
+console.log('testings sdfdsf');
+console.log('testing');
+console.log('testing');
+console.log('hello world');
+}
--- test.js
+++
@@ -1,5 +0,0 @@
-console.log('testing);
-console.log('testing new branch');
-console.log('testings sdfdsf');
-console.log('testing);
-console.log('testing);
